9fde518622fdef25530756262a953780032f1e62
mlondong <mauricio.londono@gmail.com>
2021-08-16 18:34:30 -0500

    change interfaces to add JsonSubType

--- src/main/java/co/com/foodbank/vehicule/dto/interfaces/IVehicule.java
+++ src/main/java/co/com/foodbank/vehicule/dto/interfaces/IVehicule.java
@@ -1,10 +1,18 @@
 package co.com.foodbank.vehicule.dto.interfaces;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import co.com.foodbank.vehicule.dto.Vehicule;
 
 /**
  * @author mauricio.londono@gmail.com co.com.foodbank.vehicule.dto 16/05/2021
  */
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY,
+        property = "type")
+@JsonSubTypes({@Type(value = Vehicule.class, name = "Vehicule")})
 public interface IVehicule {
     public IVolume getVolume();
 
